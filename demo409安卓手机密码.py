c1bf4b78a238d1597f313c745b8ded325e43da23
ZhaoRv <1933252548@qq.com>
2025-04-11 01:22:42 +0800

    410代码联系

--- demo409安卓手机密码.py
+++ demo409安卓手机密码.py
@@ -2,11 +2,15 @@ a[0]=[0,0,0]
 a[1]=[1,2,3]
 a[2]=[4,5,6]
 a[3]=[7,8,9]
-while not (mima=input())==None:
+while True:
+    mima = input()
+    name = input()
+    if mima == "":
+        break
     def chongfu(mima):
         if len(set(mima)) == len(mima):
-        print("NO")
-        return False
+            print("NO")
+            return False
         else:
             return True
     def find_2d_index(lst, value,value_next):
@@ -18,7 +22,6 @@ while not (mima=input())==None:
                 n = row.index(value_next)
             if i+1!=m or  i-1!=m or j+1!=n or j-1!=n:
                print("NO")
-                
                          
 
 
---
+++ demo410二叉树的左右遍历.py
@@ -0,0 +1,7 @@
+class Solution:
+    def levelOrder(self , root: TreeNode) -> List[List[int]]:
+       if not root: return 
+       else:
+        if root
+            
+        
